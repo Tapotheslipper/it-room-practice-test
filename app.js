00240ea4e40d054db364b9e718e6d9772f31650a
tapo <hypergag@gmail.com>
2025-05-29 14:31:25 +0400

    check

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
-import http from 'http'; // importing the built-in http module
-const server = http.createServer((request, response) => {
-    response.end("Hello NodeJS");
-});
+// import http from 'http'; // importing the built-in http module
+// const server = http.createServer((request, response) => {
+//     response.end("Hello NodeJS");
+// });
 
-server.listen(3001, '127.0.0.1', () => {
-    console.log("Server started on PORT :3001");
-});
+// server.listen(3001, '127.0.0.1', () => {
+//     console.log("Server started on PORT :3001");
+// });
+
+import {square, cube} from './math.js';
+import {open, read, close} from 'fs';
+import _ from 'underscore';
+import lodash from 'lodash';
+
+let index = 1;
+
+console.log(toString('23'));
