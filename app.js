db4d21aadb9c968bb4de010c7e0ee4b72178ca90
tapo <hypergag@gmail.com>
2025-05-30 15:14:45 +0400

    set

--- app.js
+++ app.js
@@ -6,3 +6,16 @@ const server = http.createServer((request, response) => {
 server.listen(3001, '127.0.0.1', () => {
     console.log("Server started on PORT :3001");
 }); */
+
+import express from 'express';
+
+const app = express();
+const port = 3000;
+
+app.get('/', (request, response) => {
+    response.send('Ebal Baranov.');
+});
+
+app.listen(port, () => {
+    console.log('Example app listening on port ', port);
+});
--- math.js
+++
@@ -1,7 +0,0 @@
-export function square(num) {
-    return Math.pow(num, 2);
-}
-
-export function cube(num) {
-    return Math.pow(num, 3);
-}
