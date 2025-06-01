160f006a39a935050c354d3b93379b7737fabcfc
tapo <hypergag@gmail.com>
2025-06-01 21:12:36 +0400

    it finally works!

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import fs from 'fs';
 
 const app = express();
 const port = 3000;
@@ -9,12 +10,29 @@ app.use(bodyParser.json());
 
 let posts = [];
 
-// getting all posts
+const dbPath = './db.json';
+fs.readFile(dbPath, 'utf-8', (err, data) => {
+   if (err) {
+    console.log('error reading file: ', err);
+    return;
+   }
+   posts = JSON.parse(data).posts;
+});
+
 app.get('/api/posts', (req, res) => {
     res.json(posts);
 });
 
-// creating a post
+app.get('/api/posts/:id', (req, res) => {
+    let postId = parseInt(req.params.id);
+    let post = posts.find(p => p.id === postId);
+    if (!post) {
+        return res.status(404).send('post not found');
+    } else {
+        res.json(post);
+    }
+});
+
 app.post('/api/posts', (req, res) => {
     let newPost = {
         id: posts.length + 1,
@@ -22,33 +40,50 @@ app.post('/api/posts', (req, res) => {
         content: req.body.content
     };
     posts.push(newPost);
+    fs.writeFile(dbPath, JSON.stringify({posts}, null, 2), (err) => {
+        if (err) {
+            console.log('error writing in file: ', err);
+            return;
+        }
+    });
     res.status(201).json(newPost);
 });
 
-// edit a post
-app.put('api/posts', (req, res) => {
+app.put('/api/posts/:id', (req, res) => {
     let postId = parseInt(req.params.id);
-    let post = posts.find(p => p.id === postId);
-    if (!post) {
-        return res.status(404).send('Пост не найден.');
+    let postIndex = posts.findIndex(p => p.id === postId);
+
+    if (postIndex === -1) {
+        return res.status(404).send('Post not found.');
     } else {
-        post.title = req.body.title;
-        post.content = req.body.content;
-        res.json(post);
+        posts[postIndex].title = req.body.title;
+        posts[postIndex].content = req.body.content;
+        fs.writeFile(dbPath, JSON.stringify({posts}, null, 2), (err) => {
+            if (err) {
+                console.log('error changing in file: ', err);
+                return;
+            }
+        });
+        res.json(posts[postIndex]);
     }
 });
 
-// deleting a post
-app.delete('api/posts/:id', (req, res) => {
+app.delete('/api/posts/:id', (req, res) => {
     let postId = parseInt(req.params.id);
     posts = posts.filter(p => p.id !== postId);
+    fs.writeFile(dbPath, JSON.stringify({posts}, null, 2), (err) => {
+        if (err) {
+            console.log('error removing from file: ', err);
+            return;
+        }
+    });
     res.status(204).send();
 });
 
 app.get('/', (req, res) => {
-    res.send('Ebal Baranov.');
+    res.send('Title page.');
 });
 
 app.listen(port, () => {
-    console.log('Example app listening on port ', port);
+    console.log('First app listening on port ', port);
 });
